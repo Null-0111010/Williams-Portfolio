970b308b680e9029f75e43d20b13a72f34336309
Null-0111010 <163085242+Null-0111010@users.noreply.github.com>
2024-03-22 21:20:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const contactForm= document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Handle form submission here
+});
